818bb9cfe5683e274019ecfdfc3cbd377c179594
Cjiaye <11204115+cjiaye@user.noreply.gitee.com>
2022-07-14 16:49:16 +0800

    fix(all): all

--- src/views/sys-users/index.vue
+++ src/views/sys-users/index.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>用户管理</div>
+  <div>
+    <el-form :inline="true" :model="loginForm" class="demo-form-inline">
+      <el-form-item label="用户名">
+        <el-input v-model="loginForm.username"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="success">查询</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="dialogTableVisible = true"
+          >新增</el-button
+        >
+      </el-form-item>
+    </el-form>
+
+    <el-dialog title="新增用户" :visible.sync="dialogTableVisible"> </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      dialogTableVisible: false,
+      loginForm: {
+        username: ''
+      }
+    }
   },
 
   mounted() {},
